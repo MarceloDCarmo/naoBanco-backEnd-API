f8a64b240f2807190591c9d10c4c86d171b36a9d
Michael Ruppelt <michaelruppelt.s@gmail.com>
2021-08-25 23:26:16 -0300

    Added Middleware of errors.

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import "reflect-metadata";
-import express from "express";
+import express, { Request, Response, NextFunction } from "express";
+import "express-async-errors";
 import { router } from "./routes";
-
 import "./database"; //From: index.ts (database)
 
 const port = 3000
@@ -9,4 +9,15 @@ const app = express()
 
 app.use(express.json());
 app.use(router);
+app.use((err: Error, request : Request, response : Response, next: NextFunction) => {
+    if(err instanceof Error){
+        return response.status(400).json({
+            error: err.message
+        })
+    }
+    return response.status(500).json({
+        status: "error",
+        message: "Internal Server Error"
+    })
+})
 app.listen(port, () => console.log(`Server running on port ${port}`));

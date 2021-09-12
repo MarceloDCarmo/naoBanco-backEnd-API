75c06ed54988c91627d0e0cc0d9fb5d326e5ca05
Michael Ruppelt <michaelruppelt.s@gmail.com>
2021-09-12 16:33:33 -0300

    Revert "Revert "Add Adjustments to Token Validation""
    This reverts commit 9983229b45c393a2f8c16e35b95cbe47ae3183a8.

--- src/middlewares/ensureAuthenticated.ts
+++ src/middlewares/ensureAuthenticated.ts
@@ -1,6 +1,10 @@
 import {Request, Response, NextFunction} from "express";
 import { verify } from "jsonwebtoken";
 
+interface IPayLoad{
+    sub: string;
+}
+
 export function ensureAuthenticated(
     request: Request,
     response: Response,
@@ -14,15 +18,13 @@ export function ensureAuthenticated(
     const [,token] = authToken.split(" ");
 
     try {
-        const decode = verify(token, "eaa474ac8948940e31539fcfba1cfa6f")
-        //request.user_id = sub; // add Later (config types express)
+        const {sub} = verify(token, "eaa474ac8948940e31539fcfba1cfa6f") as IPayLoad;
+        request.user_id = sub; // add Later (config types express)
 
         return next();
     }catch(err){
         return response.status(401).end();
     }
 
-
-    
     return next();
 }

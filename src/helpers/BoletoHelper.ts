b6c07607be15b323f113188a875c80025fe69c15
Marcelo Dias <marcelodkarmo@gmail.com>
2021-10-05 20:46:50 -0300

    Boleto number generator

---
+++ src/helpers/BoletoHelper.ts
@@ -0,0 +1,24 @@
+class BoletoHelper {
+
+    generate(issuerAccount:number, value:number, dueDate:Date):string{
+        let boletoNumber:string
+        
+        boletoNumber = 'BOL923571113171923293137'
+        boletoNumber = boletoNumber.concat(issuerAccount.toString())
+        boletoNumber = boletoNumber.concat('7')
+
+        const baseDate = new Date(1997, 10, 7)
+        let dueFactor = dueDate.getTime() - baseDate.getTime()
+        dueFactor = Math.floor(dueFactor / 86400000) //milliseconds to days
+
+        boletoNumber = boletoNumber.concat(dueFactor.toString())
+
+        let valueString = value.toString().padStart(10 ,'0')
+
+        boletoNumber = boletoNumber.concat(valueString)
+        
+        return boletoNumber
+    }
+}
+
+export { BoletoHelper }

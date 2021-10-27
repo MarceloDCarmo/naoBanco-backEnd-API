85fc9d0399e23ff1eb0347cb53717a1a52aad286
Marcelo Dias <marcelodkarmo@gmail.com>
2021-10-27 16:37:03 -0300

    Change port

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,7 @@ import "express-async-errors";
 import { router } from "./routes";
 import "./database"; //From: index.ts (database)
 
-const port = 3000
+const port = 8080
 const app = express()
 
 app.use(express.json());
